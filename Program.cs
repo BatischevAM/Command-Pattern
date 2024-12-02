19db20fb13a337fc7afcae107552d20284d032e6
BatischevAM <batischevam@mer.ci.nsu.ru>
2024-12-02 11:50:37 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,28 +7,18 @@ class Program
     static void Main(string[] args)
     {
         TextEditor editor = new TextEditor();
-
-        // Ввод текста
+        
         ICommand insertHello = new InsertTextCommand(editor, "Hello");
         editor.ExecuteCommand(insertHello);
-
         ICommand insertWorld = new InsertTextCommand(editor, " World");
         editor.ExecuteCommand(insertWorld);
 
-        // Удаление текста
         ICommand deleteText = new DeleteTextCommand(editor, 6); // Удаляем " World"
         editor.ExecuteCommand(deleteText);
 
-        // Отмена последней операции (удаления)
         editor.Undo();
-
-        // Отмена последней операции (вставки " World")
         editor.Undo();
-
-        // Отмена последней операции (вставки "Hello")
         editor.Undo();
-
-        // Попытка отмены без доступных операций
         editor.Undo();
     }
 }
